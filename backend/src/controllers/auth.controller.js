218ed6ec59b257062c7c4087ae2b0d7aa2caebae
Jitendra Kumar <jitendra0202006@gmail.com>
2026-07-08 20:26:56 +0530

    User Register ControllerComplete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import {userModel} from "../models/user.model.js";
+import bcrypt from "bcryptjs"
 
 export async function userRegisterController(req, res){
-    const {user, email, password} = req.body;
+    const {username, email, password} = req.body;
+
+    if(!username || !email || !password){
+        return res.status(401).json({
+            message: "User's Credentials are necessary"
+        })
+    }
 
     const isUserAlreadyExists = await userModel.findOne({
-        $or: [{user}, {email}]
+        $or: [{username}, {email}]
     })
 
     if(isUserAlreadyExists){
@@ -12,6 +19,32 @@ export async function userRegisterController(req, res){
             message: "User is already exists with this account"
         })
     }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await userModel.create({
+        username, 
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET,{
+        expiresIn: '6d'
+    })
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+        message: "User Registered Successfully",
+        user: {
+            username: user.username,
+            id: user._id,
+            email: user.email
+        },
+        token
+    })
 
     
 }
